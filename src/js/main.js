cc3026bc98b736595f7810580f3cda090b4ad496
RuStyC0der <miller_maers@tutanota.com>
2019-11-19 08:42:04 +0200

    added slider

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$('.slider__items').slick({
+    slidesToShow: 2,
+    slidesToScroll:4,
+    infinite: true,
+    prevArrow: '.slider__arrow_left',
+    nextArrow: '.slider__arrow_right'
+});
+});
+
